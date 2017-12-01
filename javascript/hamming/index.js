e16e532ed576b5293a6151e65c93dae211523b48
Spencer Cooley <contact@spencercooley.com>
2017-12-01 13:52:54 -0800

    solved code challenge with both python and javascript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,23 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+class Hamming{
+    compute(strandA,strandB){
+      var mutationCount = 0;
+      //ensure the strands are the same length
+      if(strandA.length != strandB.length){
+        throw 'DNA strands must be of equal length.'
+      }
+      else{
+        //count the difference in nucleotides between strands
+        for(var i = 0; i < strandA.length; i++) {
+          if(strandA[i] != strandB[i]){
+            mutationCount++
+          }
+        }
+        return mutationCount
+      }
+    }
+}
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,14 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(a,b):
+    # ensure the strands are the same length
+    if len(a) != len(b):
+        raise ValueError('strains must be the same length')
+    else:
+        mutation_count = 0
+        # count the difference in nucleotides between strands
+        for i, nucleotide in enumerate(a):
+            if nucleotide != b[i]:
+                mutation_count += 1
+        return mutation_count
